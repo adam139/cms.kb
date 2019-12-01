e663156d0187b3754efdc444308dc345fc35143d
adam139 <yuejun.tang@gmail.com>
2019-12-01 17:08:11 +0800

    add events interface

--- cms/db/interfaces.py
+++ cms/db/interfaces.py
@@ -6,25 +6,28 @@ from zope import schema
 
 from cms.db import _
 
-class IObjCreated(Interface):
+class IRecorderCreated(Interface):
     """当关系数据库创建记录是,触发该事件,通知Plone创建关联的富文本对象"""
-    id = Attribute("object id")
-    title = Attribute("object title") 
+    id = Attribute("Recorder id")
+    cls = Attribute("the Recorder relative content type class") 
 
-class IObjDeleted(Interface):
+class IRecorderDeleted(IRecorderCreated):
     """当关系数据库删除某记录时,触发该事件,通知Plone删除关联对象"""
-    id = Attribute("object id")
-    title = Attribute("object title") 
+ 
 
-class IDanWeiRecorderCreated(IObjCreated):
+class IDanWeiRecorderCreated(IRecorderCreated):
     """当关系数据库创建一条单位记录时,触发该事件,通知Plone创建包含富文本对象的单位"""
 
-class IYiShengRecorderCreated(IObjCreated):
+class IYiShengRecorderCreated(IRecorderCreated):
     """当关系数据库创建一条医生记录时,触发该事件,通知Plone创建包含富文本对象的医生"""
 
-class IBingrenRecorderCreated(IObjCreated):
+class IBingrenRecorderCreated(IRecorderCreated):
     """当关系数据库创建一条bingren记录时,触发该事件,通知Plone创建包含富文本对象的bingren"""
     
+class IAutomaticTypesSettings(Interface):
+    """automatic create content types setting"""
+    
+
 class ISysSettings(Interface):
     """ system settings
     """         
