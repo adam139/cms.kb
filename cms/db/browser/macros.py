276613da70995dee81309ceb71b8dd0119e098d0
adam139 <yuejun.tang@gmail.com>
2020-01-02 16:28:29 +0800

    add navigator macro to display template

--- cms/db/browser/macros.py
+++ cms/db/browser/macros.py
@@ -1,4 +1,5 @@
-# Five imports
+#-*- coding: UTF-8 -*-
+from plone import api
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.memoize.instance import memoize
@@ -59,6 +60,52 @@ class Macros(BrowserView):
         pt = getToolByName(self.context, 'portal_transforms')
         return pt.convertTo('text/html', text, mimetype='text/plain').getData()
 
+    @memoize
+    def getorm_url(self):
+        ""
+        base = self.getobj_url("cms.db.ormfolder")
+        return base
+
+    def bingren_folder_url(self):
+        ""
+        base = self.getobj_url("cms.db.bingrenfolder")
+        return base
+    
+    @memoize
+    def yao_folder_url(self):
+        ""
+        base = self.getobj_url("cms.db.yaofolder")
+        return base
+    
+    def yisheng_folder_url(self):
+        ""
+        base = self.getobj_url("cms.db.yishengfolder")
+        return base
+    
+    def danwei_folder_url(self):
+        ""
+        base = self.getobj_url("cms.db.danweifolder")
+        return base
+
+    def wuyun_folder_url(self):
+        ""
+        base = self.getobj_url("cms.db.wuyunfolder")
+        return base
+    
+    def chufang_folder_url(self):
+        ""
+        base = self.getobj_url("cms.db.chufangfolder")
+        return base        
+            
+        
+    def getobj_url(self,type):
+        catalog = api.portal.get_tool('portal_catalog')
+        brains = catalog(portal_type=type)
+        if bool(brains):
+            return brains[0].getURL()
+        else:
+            return ''    
+    
     def timedelta(self, start, end):
         if isinstance(start, DateTime):
             start=start.asdatetime()
