1c3d66508abc7a83305f6aa99c10b8bafab5abb3
adam139 <yuejun.tang@gmail.com>
2020-01-10 14:30:24 +0800

    add two output filter functions

--- cms/db/dbutility.py
+++ cms/db/dbutility.py
@@ -42,6 +42,22 @@ def map_yao_chufang_list(recorder):
     else:                
         return ",".join([mingcheng,yaoliang])
 
+def ex_map_yao_chufang_list(recorder):
+    "chufang_listings view map function,"
+    "provide a specify output format"
+    "parameter:recorder is a association table object recorder"
+
+    yao_id = recorder.yao_id
+    yao = Session.query(Yao).filter(Yao.id ==yao_id).one()
+    mingcheng = getattr(yao,'mingcheng',"")
+    yaoliang = u"%s克" % recorder.yaoliang
+    paozhi = recorder.paozhi
+    if bool(paozhi):
+        paozhi = "(%s)" % recorder.paozhi
+        return ",".join([mingcheng,yaoliang,paozhi])
+    else:                
+        return ",".join([mingcheng,yaoliang])
+
 def map_yao_chufang_total(recorder):
     "chufang's base_view map function,"
     "provide a total output format"
@@ -56,6 +72,39 @@ def map_yao_chufang_total(recorder):
         xiaoji = str(yaoliang * danjia)
         return xiaoji
     return str(0.00)
+
+def ex_map_yao_chufang_total(recorder):
+    "chufang's base_view map function,"
+    "provide a total output format"
+    "parameter:recorder is a association table object recorder"
+    "recorder like:(u'\u767d\u828d', 7L,'wu', 0.26,700)"
+
+
+    danjia = recorder[3]
+
+    yaoliang = recorder[1]
+    if bool(danjia):
+        xiaoji = str(yaoliang * danjia)
+        return xiaoji
+    return str(0.00)
+
+def ex_map_yao_chufang_danwei(recorder):
+    "chufang's base_view map function,"
+    "provide a specify output format"
+    "parameter:recorder is a yao_chufang association table object recorder"
+    "recorder like:(u'\u767d\u828d', 7L,'wu', 0.26,700)"
+
+
+    mingcheng = recorder[0]
+    yaoliang = u"%s克" % recorder[1]
+    paozhi = recorder[2]
+    
+    if bool(paozhi):
+#         paozhi = "(%s)" % recorder.paozhi
+        return "<td>%s</td><td>%s</td><td>%s</td>" % (mingcheng,yaoliang,paozhi)
+    else:                
+        paozhi = u"无"
+        return "<td>%s</td><td>%s</td><td>%s</td>" % (mingcheng,yaoliang,paozhi)
 
 def map_yao_chufang_table(recorder):
     "chufang's base_view map function,"
