67acdddaae28797326481bcf9ea348883617d565
adam139 <yuejun.tang@gmail.com>
2020-01-22 15:46:04 +0800

    enhance db api interface

--- cms/db/interfaces.py
+++ cms/db/interfaces.py
@@ -41,6 +41,18 @@ class InputError(Exception):
 class IDbapi (Interface):
     """Db api """
 
+    def __init__(session,package,table,factorycls,columns=None,fullsearch_clmns=None):
+        """
+        parameters:
+        :session db mapper session,
+        :package the package where table class in here. for example:'cms.db.orm',
+        :table the database table name that will be query, 'admin_logs',
+        :factorycls the class name that will be create table object instance,'AdminLog',
+         or the class itself,AdminLog.
+        :columns will be returned table columns,
+        :fullsearch_clmns the columns that will been used keyword full text search
+        """    
+    
     def init_table():
         "import the table's mapper class named tablecls"
         
@@ -87,6 +99,90 @@ class IDbapi (Interface):
         :searchcnd:the search condition that located the recorder, type: dict
         """    
 
+    def search_clmns2sqltxt(self,clmns):
+        """get columns that will been used keyword full text search
+        :input:clmns = ['tit','des']
+        :output:" tit LIKE :x OR des LIKE :x "                                  
+        """
+
+    def search_clmns4filter(clmns,tablecls,keyword):
+        """get columns that will been used keyword full text search
+        :input:clmns = ['tit','des']
+        :input:tablecls = mapper object
+        :input:keyword search keyword:string
+        :output:" [getattr(tablecls, 'tit').like("%" + query + "%"),getattr(tablecls, 'des').like("%" + query + "%")] "
+                                  
+        """
+
+    def pk_title(pk,factorycls,title):
+        """primary key to row recorder 's title
+        根据主键提取指定表对象的属性
+        """
+
+    def pk_obj_property(pk,rpt,title):
+        """primary key to row recorder 's title
+        根据主键查本表的关系属性,提取该关系属性指向的对象的指定属性
+        parameters:
+        pk:primary key:Long
+        rpt:relative property:string
+        title:target object's property:string
+        """
+
+    def pk_ass_title(pk,factorycls,asso,targetcls,fk,title):
+        """通过主键查关联表,获取多对多的对象属性 
+        #pk本地表主键  integer
+        #factorycls 本地表类 cls
+        #asso 关联表类  cls
+        #targetcls 目标表类 cls 
+        #fk关联表指向目标表外键名称 string
+        #title目标表字段/属性    string
+        """    
+
+    def pk_ass_recorders(pk,factorycls,asso):
+        """通过主键查关联表,获取多个关联表对象 
+        #pk本地表主键  integer
+        #factorycls 本地表类 cls
+        #asso 关联表类  cls
+        """    
+ 
+    def pk_ass_obj_title(pk,factorycls,asso,targetcls,fk,title,mapf):
+        """通过主键查关联表对象,获取多对多关联对象的属性 
+        #pk本地表主键  integer
+        #factorycls 本地表类 cls
+        #asso 关联表类   cls
+        #targetcls 目标表类  cls
+        #fk关联表指向目标表外键名称 string
+        #title目标表字段/属性    string
+        #mapf 映射函数   function
+        """
+
+    def ex_pk_ass_obj_title(pk,factorycls,asso,asso_p1,asso_p2,midcls,midp,\
+                            asso2,asso2_p1,asso2_p2,p2,fk,comp):
+        """通过主键查关联表对象1,连接关联表2,获取多对多关联对象的属性 
+        #pk本地表主键  integer
+        #factorycls 本地表类 cls
+        #asso 关联表1类   cls
+        #asso_p1 关联表asso要提取的字段/属性    string
+        #asso_p2 关联表asso要提取的字段/属性    string
+        #p1目标表字段/属性    string                
+        #midcls 中间表类
+        #midp 中间表要提取的字段/属性    string
+        #asso2 关联表2类  cls
+        #asso2_p1 关联表asso2要提取的字段/属性    string
+        #asso2_p2 关联表asso2要提取的字段/属性    string
+        #p2关联表asso2字段/属性    string
+        #fk关联表asso2对应p2的外键值 int
+        #comp 两个关联表共有的主键字段/属性    string
+        """
+    
+    def get_asso_obj(cns,cls=None):
+        """query association object table 
+        parameters:
+        :cns:{'column1':'value1',...}
+        :cls: will be queryed association table mapper object
+        """
+    
+    
     def get_columns():
         "get all columns of single table"
         
