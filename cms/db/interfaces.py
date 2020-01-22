de158a6e6e0942fb8f9488f4de301df7ea34433d
adam139 <yuejun.tang@gmail.com>
2020-01-22 11:22:06 +0800

    update dbapi inteface

--- cms/db/interfaces.py
+++ cms/db/interfaces.py
@@ -41,13 +41,103 @@ class InputError(Exception):
 class IDbapi (Interface):
     """Db api """
 
+    def init_table():
+        "import the table's mapper class named tablecls"
+        
     def get_rownumber():
         "fetch table's rownumber"
 
     def bulk_delete():
         "bulk delete"
+        
+    def query(kwargs):
+        """single table query 
+        parameters:
+        :kwargs's keys parameters:
+            start:start location
+            size:batch size
+            keyword:full search keyword
+            direction:sort direction
+            max:batch size for Oracle
+            with_entities:if using serial number fetch recorder's columns,1 True,0 False      
+        """         
+        
+    def multi_query(kwargs,tmaper,tbl,tc,cv,key1,key2):
+        """
+        parameters:
+        :kwargs's keys parameters:
+            start:start location
+            size:batch size
+            keyword:full search keyword
+            direction:sort direction
+            max:batch size for Oracle
+            with_entities:if using serial number fetch recorder's columns,1 True,0 False
+        :tmaper: will joined table's mapper object
+        :tbl: will joined table's name
+        :tc:will be checked table column
+        :cv:the tc should equal value
+        :key1:first table primary key
+        :key:second table fk's refer to first table         
+        """
 
+    def update_asso_table(kwargs,searchcnd):
+        """
+        parameters:
+        :kwargs:will be update data :type dict
+        :searchcnd:the search condition that located the recorder, type: dict
+        """    
+
+    def get_columns():
+        "get all columns of single table"
+        
+    def join_columns(maper):
+        "get two tables all columns when base table join the table that is provided by maper parameter"        
+    
+    def add(kwargs):
+        "add single table recorder"
+    
+    def add_multi_tables(kwargs,fk_tables,asso_tables=[],asso_obj_tables=[]):
+        """
+        update all fk_tables,asso_tables and asso_obj_tables when add base table recorder
+        fk_tables:[(pk,map_cls,attr),...]
+        asso_tables:[([pk1,pk2,...],map_cls,attr),...]
+        asso_obj_tables:{"asso_proxy_property1":[(pk,pk_cls,pk_attr,asso_cls,asso_attr,[property1,property2,...]),...],
+                            "asso_proxy_property2":[(pk,pk_cls,pk_attr,asso_cls,asso_attr,[property1,property2,...]]
+                            ,...}        
+        """
+    
+    def update_multi_tables(kwargs,fk_tables=[],asso_tables=[],asso_obj_tables=[]):
+
+        """
+        update all fk_tables,asso_tables and asso_obj_tables when  update base table properties
+        fk_tables:[(pk,map_cls,attr),...]
+        asso_tables:[([pk1,pk2,...],map_cls,attr),...]
+        asso_obj_tables:{"asso_proxy_property1":[(pk,pk_cls,pk_attr,asso_cls,asso_attr,[property1,property2,...]),...],
+                            "asso_proxy_property2":[(pk,pk_cls,pk_attr,asso_cls,asso_attr,[property1,property2,...]]
+                            ,...}
+        """
+        
+    
     def fetch_oldest():
         "fetch the oldest recorder from db"
+        
+    def getByCode(id):
+        "get the recorder by table's primary key id"
+        
+    def DeleteByCode(id):
+        "delete the recorder"
+        
+    def deleteByKwargs(kwargs):
+        "delete the first recorder where obey conditions that is provided by kwargs dictionary"
+        
+    def updateByCode(kwargs):
+        "update the recorder by id that is provided by kwargs"
+        
+    def getByKwargs(**args):
+        "get first recorder by multiple conditions that is provided by args dictionary"
+        
+    def getMultiByKwargs(**args):
+         "get all recorders by multiple conditions that is provided by args dictionary"
+        
         
         
